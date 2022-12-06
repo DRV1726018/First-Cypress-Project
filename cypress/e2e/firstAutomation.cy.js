d547e5ca06c819433c1ea41a71dfa43a6cc31ab9
davidrv <davidramirezvillegas@gmail.com>
2022-12-06 14:09:05 -0500

    Adding a test usite to automation road

--- cypress/e2e/firstAutomation.cy.js
+++ cypress/e2e/firstAutomation.cy.js
@@ -1,5 +1,31 @@
-describe('empty spec', () => {
-  it('passes', () => {
+// Prueba de logeo
+describe('Test suite', () => {
+  it('Login', () => {
     cy.visit('https://www.demoblaze.com/')
+    cy.get('#login2').click()
+    cy.get('#loginusername').type('user232')
+    cy.get('#loginpassword').type('password')
+    cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
+  })
+
+  // Prueba existencia de un botón de logout
+  it('Logout', () => {
+    cy.visit('https://www.demoblaze.com/')
+    cy.get('#login2').click()
+    cy.get('#loginusername').type('user232')
+    cy.get('#loginpassword').type('password')
+    cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
+    cy.get('#logout2').contains('Log out')
+  })
+
+  // Prueba de que un botón contiene lo que debería
+  it('El precio es de $360', () => {
+    cy.visit('https://www.demoblaze.com/prod.html?idp_=1')
+    cy.get('.price-container').should('have.value', '$360')
+
   })
 })
+
+
+
+
