aee5e216b5797277419aa7b6ad472339a304d08f
davidrv <davidramirezvillegas@gmail.com>
2022-12-06 15:48:11 -0500

    Test suite 2.0

--- cypress/e2e/firstAutomation.cy.js
+++ cypress/e2e/firstAutomation.cy.js
@@ -20,9 +20,12 @@ describe('Test suite', () => {
 
   // Prueba de que un botón contiene lo que debería
   it('El precio es de $360', () => {
-    cy.visit('https://www.demoblaze.com/prod.html?idp_=1')
-    cy.get('.price-container').should('have.value', '$360')
-
+    cy.visit('https://www.demoblaze.com/')
+    cy.get('#login2').click()
+    cy.get('#loginusername').type('user232')
+    cy.get('#loginpassword').type('password')
+    cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
+    cy.get(':nth-child(1) > .card > .card-block > h5').should('have.text', '$360')
   })
 })
 
