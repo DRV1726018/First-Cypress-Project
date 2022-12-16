a5d29441ba8322dc9148488eb13e68d3e063e058
davidrv <davidramirezvillegas@gmail.com>
2022-12-16 14:20:56 -0500

    Hola

--- cypress/e2e/firstAutomation.cy.js
+++ cypress/e2e/firstAutomation.cy.js
@@ -21,10 +21,6 @@ describe('Test suite', () => {
   // Prueba de que un botón contiene lo que debería
   it('El precio es de $360', () => {
     cy.visit('https://www.demoblaze.com/')
-    cy.get('#login2').click()
-    cy.get('#loginusername').type('user232')
-    cy.get('#loginpassword').type('password')
-    cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
     cy.get(':nth-child(1) > .card > .card-block > h5').should('have.text', '$360')
   })
 })
